28a282156d8d82d07a74f67b7d4ca94f605182eb
fukamikensuke <kensuke8293@gmail.com>
2023-02-16 05:25:54 +0900

    feat : Roomstruct 追加

--- server.go
+++ server.go
@@ -1,33 +1,45 @@
 package main
 
 import (
-	"log"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
 
+type Room struct {
+	Password string `json:"password"`
+	PaticNum int    `json:"particNum"`
+}
+
 func main() {
-	// --------------------
-
-	// Rooting
-
-	// --------------------
+	// インスタンスを作成
 	e := echo.New()
-	e.Use(middleware.CORS())
 
-	e.GET("/", home)
+	// ミドルウェアを設定
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
 
-	// Start HTTP server.
-	if err := e.Start(":8080"); err != http.ErrServerClosed {
-		log.Fatal(err)
-	}
+	// ルートを設定
+	e.GET("/myPage", hello) // ローカル環境の場合、http://localhost:1323/ にGETアクセスされるとhelloハンドラーを実行する
+
+	//e.POST("/createRoom", createRoom)
+	// サーバーをポート番号1323で起動
+	e.Logger.Fatal(e.Start(":1323"))
 }
 
-// e.GET("/", home)
-func home(c echo.Context) error {
-	return c.String(http.StatusOK, "Hello Golang + echo!")
+// ハンドラーを定義
+func hello(c echo.Context) error {
+
+	var fukami string = "Kensuke"
+	return c.String(http.StatusOK, fukami)
 }
 
-//test
+// func createRoom(c echo.Context) error {
+// 	password := c.FormValue("password")
+// 	particNum := c.FormValue("particNum")
+// 	int_particNum, _ := strconv.Atoi(particNum)
+
+// 	useDB.CreateRoom(password, int_particNum, 0, "step")
+// 	return c.String(http.StatusOK, "OK")
+// }
