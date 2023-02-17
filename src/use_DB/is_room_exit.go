b06e22abdb544fca022e3a138179a95e7e11ce9e
fukamikensuke <kensuke8293@gmail.com>
2023-02-17 23:02:28 +0900

    fix: IsRoomExitの不具合修正

--- src/use_DB/is_room_exit.go
+++ src/use_DB/is_room_exit.go
@@ -7,23 +7,24 @@ import (
 
 func IsRoomExit(id string, password string) bool {
 	ctx, client := connnectDB()
-	docRef := client.Collection("your-collection").Doc(id)
-	query := docRef.Collection("room").Where("password", "==", password)
+	docRef := client.Collection("Room").Doc(id)
 
-	docs, err := query.Documents(ctx).GetAll()
+	docSnapshot, err := docRef.Get(ctx)
 	if err != nil {
 		log.Fatalf("Failed to get document: %v", err)
 	}
 
-	if len(docs) > 0 {
-		fmt.Println("ドキュメントが存在します")
-		defer client.Close()
-		return true
-	} else {
-
-		fmt.Println("ドキュメントが存在しません")
-		defer client.Close()
-		return false
+	if docSnapshot.Exists() {
+		data := docSnapshot.Data()
+		// 指定したフィールドの値を取得する
+		value, ok := data["password"]
+		if ok && value == password {
+			fmt.Println("ドキュメントが存在し、指定したフィールドの値が一致します")
+			return true
+		}
 	}
+
+	fmt.Println("ドキュメントが存在しない、または指定したフィールドの値が一致しません")
+	return false
 
 }
