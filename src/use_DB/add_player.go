36b729cba1e407272cc5bc875337b9e6c41f0f1b
fukamikensuke <kensuke8293@gmail.com>
2023-02-19 01:00:07 +0900

    feat: playerデータの入力部分の実装

--- src/use_DB/add_player.go
+++ src/use_DB/add_player.go
@@ -1,7 +1,13 @@
 package useDB
 
 import (
+	"context"
+	"fmt"
 	"log"
+
+	"cloud.google.com/go/firestore"
+	firebase "firebase.google.com/go"
+	"google.golang.org/api/option"
 )
 
 type Player struct {
@@ -19,8 +25,25 @@ type RoomPlayer struct {
 	Playerid string
 }
 
-func AddPlayer(roomId string, playerName string, playerIcon int) string {
+func connectDB() (context.Context, *firestore.Client, error) {
+	ctx := context.Background()
+	sa := option.WithCredentialsFile("path/to/serviceAccount.json")
+	config := &firebase.Config{ProjectID: "gotest-bc4c6"}
+	app, err := firebase.NewApp(ctx, config, sa)
+	if err != nil {
+		return nil, nil, err
+	}
 
+	client, err := app.Firestore(ctx)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return ctx, client, nil
+}
+
+func AddPlayer(roomId string, playerName string, playerIcon int) string {
+	fmt.Println("OK")
 	player := Player{
 		PlayerName: playerName,
 		Icon:       playerIcon,
@@ -31,25 +54,33 @@ func AddPlayer(roomId string, playerName string, playerIcon int) string {
 		Answer:     "noanswer",
 	}
 
-	ctx, client := connnectDB()
+	ctx, client, err := connectDB()
 
+	if err != nil {
+		log.Fatalf("failed to connect to database: %v", err)
+	}
+	fmt.Println("OK")
+	fmt.Println(client, ctx)
 	docRef, _, err := client.Collection("Player").Add(ctx, player)
-
+	fmt.Println(docRef)
+	fmt.Println("OK")
 	if err != nil {
 		// Handle any errors in an appropriate way, such as returning them.
 		log.Printf("An error has occurred: %s", err)
 	}
-
+	fmt.Println("OK")
 	roomPlayer := RoomPlayer{
 		Roomid:   roomId,
 		Playerid: docRef.ID,
 	}
+	fmt.Println("OK")
 	ref := client.Collection("RoomPlayer").NewDoc()
 	_, _err := ref.Set(ctx, roomPlayer)
 	if _err != nil {
 		// Handle any errors in an appropriate way, such as returning them.
 		log.Printf("An error has occurred: %s", _err)
 	}
+	fmt.Println("OK")
 	defer client.Close()
 	return docRef.ID
 }
