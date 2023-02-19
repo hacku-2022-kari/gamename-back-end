dabdec343740f58c17d1eeacd82bb24187bb0851
fukamikensuke <kensuke8293@gmail.com>
2023-02-19 16:21:19 +0900

    fix: コメントの削除

--- src/use_DB/is_room_exit.go
+++ src/use_DB/is_room_exit.go
@@ -16,7 +16,6 @@ func IsRoomExit(id string, password string) bool {
 
 	if docSnapshot.Exists() {
 		data := docSnapshot.Data()
-		// 指定したフィールドの値を取得する
 		value, ok := data["password"]
 		if ok && value == password {
 			fmt.Println("ドキュメントが存在し、指定したフィールドの値が一致します")
