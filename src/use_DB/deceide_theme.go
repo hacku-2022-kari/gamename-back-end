9169a073669399beaf396f003c4465e6a0563847
fukamikensuke <kensuke8293@gmail.com>
2023-02-23 00:45:11 +0900

    feat: decide_themeのstepの実装

--- src/use_DB/deceide_theme.go
+++ src/use_DB/deceide_theme.go
@@ -11,11 +11,22 @@ func DecideTheme(roomId string, howToDecideTheme int) bool {
 	if err != nil {
 		return false
 	}
-	docRef := client.Collection("Room").Doc(roomId)
-	_, _err := docRef.Set(ctx, map[string]interface{}{
+	roomRef := client.Collection("Room").Doc(roomId)
+	_, _err := roomRef.Set(ctx, map[string]interface{}{
 		"HowToDecideTheme": howToDecideTheme,
 	}, firestore.MergeAll)
 	if _err != nil {
+		return false
+	}
+	var step int = 2
+	if howToDecideTheme == 1 {
+		step = 3
+	}
+
+	_, err = client.Collection("Room").Doc(roomId).Update(ctx, []firestore.Update{
+		{Path: "Step", Value: step},
+	})
+	if err != nil {
 		return false
 	}
 	defer client.Close()
