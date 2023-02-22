804d9519f593f6ab9ca0fde0b9059ee957f8cfa4
fukamikensuke <kensuke8293@gmail.com>
2023-02-22 23:20:42 +0900

    fix: helloの追加

--- server.go
+++ server.go
@@ -51,7 +51,7 @@ func main() {
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 	// ローカル環境の場合、http://localhost:1323/
-	e.GET("/", hello)
+	e.GET("/hello", hello)
 	e.GET("/is-room-exit", isRoomExit)
 	e.GET("/is-room-exit", isRoomExit)
 	e.GET("/partic-list", func(c echo.Context) error { //TODO関数の管理ときに修正
