cb0ddaea26e4b943af2207793897f7cb28c4a486
fukamikensuke <kensuke8293@gmail.com>
2023-02-23 01:23:26 +0900

    feat: create_themeのstepの増加

--- src/use_DB/create_theme.go
+++ src/use_DB/create_theme.go
@@ -1,33 +1,59 @@
 package useDB
 
 import (
-	"fmt"
 	"log"
 
 	"cloud.google.com/go/firestore"
 )
 
-func CreateTheme(inputTheme string, id string) bool {
+func CreateTheme(inputTheme string, playerId string, roomId string) bool {
 
 	ctx, client, err := connectDB()
 
 	if err != nil {
 		log.Fatalf("failed to connect to database: %v", err)
 	}
-	docRef := client.Collection("Player").Doc(id)
-	fmt.Println(id)
+
+	docRef := client.Collection("Player").Doc(playerId)
 	_, _err := docRef.Set(ctx, map[string]interface{}{
-		"Theme": inputTheme,
+		"Theme":  inputTheme,
+		"Status": true,
 	}, firestore.MergeAll)
 	if _err != nil {
 		log.Fatalf("failed to connect to database: %v", _err)
 	}
+
+	rpQuery := client.Collection("RoomPlayer").Where("RoomId", "==", roomId)
+	rpDocs, err := rpQuery.Documents(ctx).GetAll()
+	var okCount int = 1
+	for _, rpDoc := range rpDocs {
+		playerID := rpDoc.Data()["PlayerId"].(string)
+		playerDoc, err := client.Collection("Player").Doc(playerID).Get(ctx)
+		if err != nil {
+			log.Fatalf("error getting Player document: %v\n", err)
+		}
+		if int(playerDoc.Data()["Role"].(int)) != 1 {
+			if rpDoc.Data()["Hint"].(string) != "no-hint" {
+				okCount += 1
+			}
+		}
+	}
+
+	rdoc, err := client.Collection("Room").Doc(roomId).Get(ctx)
+	if err != nil {
+		return false
+	}
+	if okCount == rdoc.Data()["PaticNum"] {
+		_, err = client.Collection("Room").Doc(roomId).Update(ctx, []firestore.Update{
+			{Path: "Step", Value: 3},
+		})
+		if err != nil {
+			return false
+		}
+	}
 	defer client.Close()
 	return true
 }
-
-
-
 
 // $body = @{
 
