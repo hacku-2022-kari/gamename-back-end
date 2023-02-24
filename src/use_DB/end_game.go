796980b8bc9762fc28294128c9821bab70f1996d
Kensuke Fukami <kensuke8293@gmail.com>
2023-02-24 17:58:50 +0900

    feat: end-gameの実装

--- src/use_DB/end_game.go
+++ src/use_DB/end_game.go
@@ -12,12 +12,16 @@ func EndGame(roomId string) bool {
 		return false
 	}
 	roomRef := client.Collection("Room").Doc(roomId)
+	roomDoc, err := client.Collection("Room").Doc(roomId).Get(ctx)
 	_, _err := roomRef.Set(ctx, map[string]interface{}{
 		"Answer":           "no-answer",
 		"Step":             0,
 		"HowToDecideTheme": 0,
 		"IsCorrect":        false,
 		"Theme":            "no-theme",
+		"IsMoodWolf" :		 roomDoc.Data()["WolfMode"].(bool),
+		"IsExitWolf" : 		false,
+		"PeaceVote"  :		0,
 	}, firestore.MergeAll)
 	if _err != nil {
 		return false
@@ -33,12 +37,16 @@ func EndGame(roomId string) bool {
 	for _, rpDoc := range rpDocs {
 		playerID := rpDoc.Data()["PlayerId"].(string)
 		playerRef := client.Collection("Player").Doc(playerID)
+		playerDoc,err := client.Collection("Player").Doc(playerID).Get(ctx)
 		_, err = playerRef.Set(ctx, map[string]interface{}{
 			"Answer":   "no-answer",
 			"Hint":     "no-hint",
 			"Role":     0,
 			"IsDelete": false,
 			"Theme":    "no-theme",
+			"Point":	playerDoc.Data()["Point"].(int),
+			"Wolf":		false,
+			"Vote" : 	0,
 		}, firestore.MergeAll)
 		if err != nil {
 			return false
@@ -50,6 +58,6 @@ func EndGame(roomId string) bool {
 }
 
 // $body = @{
-//     roomId = "idkAj1Km0ACPCkQybbPD"
+//     roomId = "WgBySaSIBvs92OsDdd4i"
 // } | ConvertTo-Json
 // Invoke-RestMethod -Method POST -Uri http://localhost:1323/initialize -Body $body -ContentType "application/json"
