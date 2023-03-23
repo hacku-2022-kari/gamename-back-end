067caaad0f95e30567fed840f8c9c1e51de52eea
fukamikensuke <kensuke8293@gmail.com>
2023-03-23 19:42:06 +0900

    feat: space環境の追加

--- server.go
+++ server.go
@@ -8,6 +8,7 @@ import (
 	types "gamename-back-end/pkg/types"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
@@ -50,7 +51,17 @@ func main() {
 	e.POST("/vote", postVote)
 	e.POST("/judgement-wolf", postJudgementWolf)
 	e.POST("/add-step", postAddStep)
-	e.Logger.Fatal(e.Start(":1323"))
+	e.Logger.Fatal(e.Start(getPort()))
+}
+func getPort() string {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = ":3000"
+	} else {
+		port = ":" + port
+	}
+
+	return port
 }
 func isModeWolf(c echo.Context) error {
 	roomId := c.QueryParam("roomId")
