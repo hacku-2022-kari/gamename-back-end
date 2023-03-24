16275f128d5172d6ad3e1665decd89e53a7204c1
ltoppyl <toppyajibutu@gmail.com>
2023-03-24 14:41:18 +0900

    feat: get_role_wolf に roomId 引数の追加

--- server.go
+++ server.go
@@ -96,7 +96,8 @@ func getRole(c echo.Context) error {
 }
 func getRoleWolf(c echo.Context) error {
 	playerId := c.QueryParam("playerId")
-	return c.JSON(http.StatusOK, readDB.GetRoleWolf(playerId))
+	roomId := c.QueryParam("roomId")
+	return c.JSON(http.StatusOK, readDB.GetRoleWolf(playerId, roomId))
 }
 func getAnswer(c echo.Context) error {
 	roomId := c.QueryParam("roomId")
